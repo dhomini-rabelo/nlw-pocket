dd6893d563d72ad30ee8447bbc38a304db6ee90a
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2024-12-15 11:20:09 -0300

    feat: adding Loading component

--- mobile-app/src/app/_layout.tsx
+++ mobile-app/src/app/_layout.tsx
@@ -1,7 +1,27 @@
 import { Stack } from "expo-router";
 import { colors } from "@/styles/theme";
+import { Loading } from "@/components/Loading";
+
+import {
+  useFonts,
+  Rubik_400Regular,
+  Rubik_500Medium,
+  Rubik_600SemiBold,
+  Rubik_700Bold,
+} from "@expo-google-fonts/rubik";
 
 export default function Layout() {
+  const [fontsIsLoaded] = useFonts({
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_600SemiBold,
+    Rubik_700Bold,
+  })
+
+  if (!fontsIsLoaded) {
+    return <Loading />
+  }
+
   return (
     <Stack
       screenOptions={{
